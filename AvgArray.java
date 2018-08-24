f2f686eccfb4b2345695112366b3a914a40e09be
ArfatAmer <arfatamer@gmail.com>
2018-08-24 10:40:02 +0530

    Pushing local to remote

---
+++ AvgArray.java
@@ -0,0 +1,13 @@
+class AvgArray{
+public static void main(String args[])
+{
+int ar[4]={"4","5","6","7"};
+for(int i = 0; i<4; i++)
+        {
+            sum = sum + a[i];
+ 	    avg=sum/4;
+        }
+        System.out.println("Sum is:"+sum);
+        System.out.println("Average is:"+avg);
+    }
+}
---
+++ BrightIT.java
@@ -0,0 +1,10 @@
+class BrightIT{
+public static void main(String args[])
+{
+int i;
+for(i=0;i<=10;i++)
+{
+System.out.println("Bright IT Carrier");
+}
+}
+}
